fb53909a8f94740e6d99f1073c61ede052e4f7ba
Prerak Mathur <prerak@Preraks-MacBook-Pro.local>
2022-04-23 18:06:18 +0530

    But he was a great RULER.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "021fbd842a04f1e0"; }
+/******/ 		__webpack_require__.h = function() { return "5896aea5033324c1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -5,7 +5,7 @@ import config from "../config/index.json";
 const Pricing = () => {
   const { pricing } = config;
   const { items, title } = pricing;
-  const [firstPlan, secondPlan, thirdPlan] = items;
+  const [firstPlan, thirdPlan] = items;
 
   return (
     <section className={`bg-background py-8`} id="pricing">

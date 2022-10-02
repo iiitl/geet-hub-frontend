0ac6c4a2f762b208208fd883132a58c5f138ecfa
deep-69bits <deepakchattwaniking15@gmail.com>
2022-10-02 15:33:16 +0530

    Fixing the color scheme

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f1e1d63ffdbdcb10"; }
+/******/ 		__webpack_require__.h = function() { return "49d106b7ac557cfd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
